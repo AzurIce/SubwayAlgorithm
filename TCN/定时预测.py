d8a0e173ae274d498b0bd7e79292a39300e742e1
许一涵 <81723334@qq.com>
2023-07-10 10:29:48 +0800

    TCN预测

---
+++ TCN/定时预测.py
@@ -0,0 +1,12 @@
+from subprocess import check_call
+from time import sleep
+from datetime import datetime
+
+while True:
+    hours_date = (datetime.now()).strftime("%Y%m%d%H")[-2:] # 获取小时
+    # if int(hours_date)%4==0:
+    cmd ='python predict.py'
+    print(hours_date)
+    check_call(cmd, shell=True)
+    print("...执行结束")
+    sleep(3599*4) # 跳过 目标时间段
